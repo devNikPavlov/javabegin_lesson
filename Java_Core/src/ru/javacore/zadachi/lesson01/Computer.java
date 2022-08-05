b149bc897018dccec68c7aa4e0ed1d19aadffb39
devNikPavlov <devnikolaypavlov@gmail.com>
2022-08-05 13:41:52 +0300

    Создание задачи № 1.

---
+++ Java_Core/src/ru/javacore/zadachi/lesson01/Computer.java
@@ -0,0 +1,14 @@
+package ru.javacore.zadachi.lesson01;
+
+public class Computer {
+
+    String name = "IBM";        // свойство (поле) класса
+
+    void on() {
+        System.out.println(name + " on");
+    }
+
+    void off() {
+        System.out.println(name + " off");
+    }
+}
---
+++ Java_Core/src/ru/javacore/zadachi/lesson01/Main.java
@@ -0,0 +1,12 @@
+package ru.javacore.zadachi.lesson01;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Computer computer = new Computer();
+
+        computer.on();
+        computer.off();
+    }
+}
